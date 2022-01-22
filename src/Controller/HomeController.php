86c0fe2b97711d0697eea21f2e68045fbddd24c2
Cédric Martin <c.cedric.martin@gmail.com>
2022-01-22 18:06:10 +0100

    essaie mise en place affichage perso en page daccueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use App\Repository\FrontPageNouveauPersosRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\EncyclopedieDesPersonnages;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FrontPageNouveauPersosRepository;
+use App\Repository\EncyclopedieDesPersonnagesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(FrontPageNouveauPersosRepository $frontPageNouveauPersoRepository): Response
+    public function index(EncyclopedieDesPersonnagesRepository $encyclopedieDesPersonnagesRepository): Response
     {
-        $frontPageNouveauPerso = $frontPageNouveauPersoRepository->findBy([], ['id' => 'DESC'], 6);
         return $this->render('home/index.html.twig', [
-            'frontPageNouveauPerso ' => $frontPageNouveauPerso ,
+            'encyclopedie_des_personnages' => $encyclopedieDesPersonnagesRepository->findBy(
+                array(),
+                array('id'=> 'DESC'),
+                6,
+                0
+            )
         ]);
     }
 }

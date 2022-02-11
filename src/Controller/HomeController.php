3648dfdc23c132bc67a2912eb42117a248cd3482
Cédric Martin <c.cedric.martin@gmail.com>
2022-02-11 01:14:57 +0100

    push test front page envoie a la fiche du perso

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\EncyclopedieDesPersonnages;
+use App\Repository\FichePersonnageRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\FrontPageNouveauPersoRepository;
@@ -13,9 +14,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(FrontPageNouveauPersoRepository $frontPageNouveauPersoRepository): Response
+    public function index(FichePersonnageRepository $fichePersonnageRepository, FrontPageNouveauPersoRepository $frontPageNouveauPersoRepository, int $id): Response
     {
         return $this->render('home/index.html.twig', [
+            $fichePersonnage = $frontPageNouveauPersoRepository->find($id),
+            $personnages = $fichePersonnageRepository->findBy(['fichePersonnage' => $id], ['id' => 'ASC']),
+            'fichePersonnage' => $fichePersonnage,
+            'personnages' => $personnages,
             'front_page_nouveau_perso' => $frontPageNouveauPersoRepository->findBy(
                 array(),
                 array('id'=> 'ASC'),

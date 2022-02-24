26c93d5aee32802bc9e7584e35999e81e55b6d16
Cédric Martin <c.cedric.martin@gmail.com>
2022-02-24 01:29:53 +0100

    modif bouton css connection /deco etc

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,15 +18,7 @@ class UserController extends AbstractController
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/{id}', name: 'user_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
+            'user' => $this->getUser(),
         ]);
     }
 

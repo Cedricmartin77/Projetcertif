33be4905ef69a4562f5178b5a98ad4210f0c8210
Cédric Martin <c.cedric.martin@gmail.com>
2022-02-24 22:04:40 +0100

    push pagination de encyclopédie du personnage

--- src/Controller/EncyclopedieDesPersonnagesController.php
+++ src/Controller/EncyclopedieDesPersonnagesController.php
@@ -2,22 +2,30 @@
 
 namespace App\Controller;
 
-use App\Entity\EncyclopedieDesPersonnages;
-use App\Form\EncyclopedieDesPersonnages1Type;
-use App\Repository\EncyclopedieDesPersonnagesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\EncyclopedieDesPersonnages;
+use Knp\Component\Pager\PaginatorInterface;
+use App\Form\EncyclopedieDesPersonnages1Type;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EncyclopedieDesPersonnagesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/encyclopedie/des/personnages')]
 class EncyclopedieDesPersonnagesController extends AbstractController
 {
     #[Route('/', name: 'encyclopedie_des_personnages', methods: ['GET'])]
-    public function index(EncyclopedieDesPersonnagesRepository $encyclopedieDesPersonnagesRepository): Response
+    public function index(Request $request, EncyclopedieDesPersonnagesRepository $encyclopedieDesPersonnagesRepository, PaginatorInterface $paginator): Response
     {
         $encyclopediedespersos = $encyclopedieDesPersonnagesRepository->findAll();
+
+        $encyclopediedespersos = $paginator->paginate(
+            $encyclopediedespersos, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
         return $this->render('encyclopedie_des_personnages/index.html.twig', [
             'encyclopedie_des_personnages' => $encyclopediedespersos
         ]);
--- src/Controller/EncyclopedieDuPersonnageController.php
+++ src/Controller/EncyclopedieDuPersonnageController.php
@@ -5,21 +5,29 @@ namespace App\Controller;
 use App\Entity\EncyclopedieDuPersonnage;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Form\EncyclopedieDuPersonnage1Type;
+use Knp\Component\Pager\PaginatorInterface;
+use App\Repository\FichePersonnageRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EncyclopedieDuPersonnageRepository;
 use App\Repository\EncyclopedieDesPersonnagesRepository;
-use App\Repository\FichePersonnageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/encyclopedie/du/personnage')]
 class EncyclopedieDuPersonnageController extends AbstractController
 {
     #[Route('/encyclopedie/du/personnage/{id}', name: 'encyclopedie_du_personnage', methods: ['GET'])]
-    public function show($id, EncyclopedieDuPersonnageRepository $encyclopedieDuPersonnageRepository): Response
+    public function show(Request $request, $id, EncyclopedieDuPersonnageRepository $encyclopedieDuPersonnageRepository, PaginatorInterface $paginator): Response
     {
         dump($fichesPersonnages = $encyclopedieDuPersonnageRepository->findEncyclopedieDuPersonnages($id));
+
+        dump($fichesPersonnages = $paginator->paginate(
+            $fichesPersonnages, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        ));
+
         return $this->render('encyclopedie_du_personnage/show.html.twig', [
             'encyclopedieDuPersonnages' => $fichesPersonnages,
 
